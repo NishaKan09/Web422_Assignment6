350f665504d1802bf868a3d60fcaba3f96637929
Nishantha Kanagasapabathy <nishantha.kan09@gmail.com>
2020-08-13 19:00:31 -0400

    init

--- server.js
+++ server.js
@@ -76,9 +76,8 @@ app.delete("/api/posts/:id", (req, res) => {
 // Connect to the DB and start the server
 
 data.connect().then(() => {
-        app.listen(HTTP_PORT, () => { console.log("API listening on: " + HTTP_PORT) });
-    })
-    .catch((err) => {
-        console.log("unable to start the server: " + err);
-        process.exit();
-    });
+    app.listen(HTTP_PORT, () => { console.log("API listening on: " + HTTP_PORT) });
+}).catch((err) => {
+    console.log("unable to start the server: " + err);
+    process.exit();
+});
