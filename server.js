94133dbb1165fa54e4c53c250f7a0d4ba1052943
Nishantha Kanagasapabathy <nishantha.kan09@gmail.com>
2020-08-14 13:28:37 -0400

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/web422-a6'));
+//app.use(express.static(__dirname + '/web422-a6'));
+
+app.use(express.static(__dirname));
+//__dirname = path.resolve(path.dirname(''));
+console.log("here" + __dirname);
 
 app.get('/*', function(req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/web422-a6/index.html'));
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
