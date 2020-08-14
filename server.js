4805abd12917f70df06e25c2b2f7993fcba2e8b4
Nishantha Kanagasapabathy <nishantha.kan09@gmail.com>
2020-08-14 17:23:33 -0400

    init

--- server.js
+++ server.js
@@ -1,19 +1,14 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/web422-a6'));
-
-//app.use(express.static(__dirname));
-//__dirname = path.resolve(path.dirname(''));
-//console.log("here" + __dirname);
-
+// Serve static files....
+app.use(express.static(__dirname + '/dist/web422-a4'));
+console.log(__dirname)
+    // Send all requests to index.html
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/web422-a6/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/web422-a4/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
+// default Heroku PORT
 app.listen(process.env.PORT || 8080);
